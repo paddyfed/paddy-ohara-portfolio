24f66918fc67b1d624ad611f23bbc12707b5320b
Paddy O'Hara <paddyohara@S02-AppleM1-02.local>
2025-12-08 15:13:49 +0000

    Better menu

--- show-and-hide-menu.js
+++ show-and-hide-menu.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+
+const mainMenu = document.querySelector("#mainNavMenu");
+
+function openNav() {
+	mainMenu.classList.add("menu-open")
+}
+
+function closeNav() {
+		mainMenu.classList.remove("menu-open")
+
+}
