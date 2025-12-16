e82591a64dd15d3594dc220d36e24249a2da9daf
Paddy O'Hara <paddyohara@S02-AppleM1-02.local>
2025-12-16 12:06:27 +0000

    Stars randomised

--- random-background-icons.js
+++ random-background-icons.js
@@ -1,10 +1,21 @@
 const iconStage = document.querySelector(".background-icons");
 
-for(let i = 0; i < 5; i++) {
+function getRandomNumber(max) {
+	return Math.floor(Math.random() * max);
+}
+
+function getRandomPercentage() {
+	return `${getRandomNumber(80)}%`
+}
+
+for(let i = 0; i < getRandomNumber(7); i++) {
 	const star = document.createElement("i");
 	star.classList.add("fa-solid");
 	star.classList.add("fa-star");
 	star.classList.add("fa-3x");
 
+	star.style.position = "absolute"
+	star.style.top = getRandomPercentage();
+	star.style.left = getRandomPercentage();
 	iconStage.appendChild(star);
 }
