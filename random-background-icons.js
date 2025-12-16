3aaac1a4721b40a5787beb33ab3d5298750290e9
Paddy O'Hara <paddyohara@S02-AppleM1-02.local>
2025-12-16 12:41:53 +0000

    Colour icons

--- random-background-icons.js
+++ random-background-icons.js
@@ -5,10 +5,10 @@ function getRandomNumber(max) {
 }
 
 function getRandomPercentage() {
-	return `${getRandomNumber(80)}%`
+	return `${getRandomNumber(90)}%`
 }
 
-function createElement(iconClass, iconRotate = null) {
+function createElement(iconClass, iconColor, iconRotate = null) {
 	const el = document.createElement("i");
 	el.classList.add("fa-solid");
 	el.classList.add(iconClass);
@@ -20,21 +20,22 @@ function createElement(iconClass, iconRotate = null) {
 	el.style.position = "absolute"
 	el.style.top = getRandomPercentage();
 	el.style.left = getRandomPercentage();
+	el.style.color = `var(${iconColor})`;
 	iconStage.appendChild(el);
 }
 
 for(let i = 0; i < getRandomNumber(7); i++) {
-	createElement("fa-star");
+	createElement("fa-star", "--main-accent-color");
 }
 
 for(let i = 0; i < getRandomNumber(3); i++) {
-	createElement("fa-asterisk");
+	createElement("fa-asterisk", "--main-accent-color");
 }
 
 for(let i = 0; i < getRandomNumber(3); i++) {
-	createElement("fa-play","fa-rotate-270");
+	createElement("fa-play", "--alt-accent-color", "fa-rotate-270");
 }
 
 for(let i = 0; i < getRandomNumber(3); i++) {
-	createElement("fa-circle");
+	createElement("fa-circle", "--main-color");
 }
