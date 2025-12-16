e4712b2292f2847052f26b2b107f448b006d7241
Paddy O'Hara <paddyohara@S02-AppleM1-02.local>
2025-12-16 12:25:53 +0000

    Random other icons

--- random-background-icons.js
+++ random-background-icons.js
@@ -1,21 +1,40 @@
 const iconStage = document.querySelector(".background-icons");
 
 function getRandomNumber(max) {
-	return Math.floor(Math.random() * max);
+	return Math.floor(Math.random() * (max - 1) + 1);
 }
 
 function getRandomPercentage() {
 	return `${getRandomNumber(80)}%`
 }
 
-for(let i = 0; i < getRandomNumber(7); i++) {
-	const star = document.createElement("i");
-	star.classList.add("fa-solid");
-	star.classList.add("fa-star");
-	star.classList.add("fa-3x");
+function createElement(iconClass, iconRotate = null) {
+	const el = document.createElement("i");
+	el.classList.add("fa-solid");
+	el.classList.add(iconClass);
+	el.classList.add("fa-2x");
+	
+	if(iconRotate)
+		el.classList.add(iconRotate);
 
-	star.style.position = "absolute"
-	star.style.top = getRandomPercentage();
-	star.style.left = getRandomPercentage();
-	iconStage.appendChild(star);
+	el.style.position = "absolute"
+	el.style.top = getRandomPercentage();
+	el.style.left = getRandomPercentage();
+	iconStage.appendChild(el);
+}
+
+for(let i = 0; i < getRandomNumber(7); i++) {
+	createElement("fa-star");
+}
+
+for(let i = 0; i < getRandomNumber(3); i++) {
+	createElement("fa-asterisk");
+}
+
+for(let i = 0; i < getRandomNumber(3); i++) {
+	createElement("fa-play","fa-rotate-270");
+}
+
+for(let i = 0; i < getRandomNumber(3); i++) {
+	createElement("fa-circle");
 }
