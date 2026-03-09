844a078c1b85ac6cb3051dcba7fa247537881f15
Paddy O'Hara <paddyohara@S02-AppleM1-02.local>
2026-03-09 14:45:28 +0000

    Additional form validation for email format

--- form.js
+++ form.js
@@ -1,45 +1,68 @@
 // JavaScript Document
-
+// find the form element
 const form = document.querySelector("form");
 
+// function for validating fields on the form
 function validateField(field) {
-    const errorElement = field.parentElement.querySelector(".form-error-message")
-    
-    if(!field.validity.valid) {
-        errorElement.textContent = "This field is required";
-        return false;
+  // find the element to display the error message
+  const errorElement = field.parentElement.querySelector(".form-error-message")
+
+  // if the field is not valid
+  if (!field.validity.valid) {
+    // change the error message
+    // If the field is required
+    if (field.validity.valueMissing) {
+      errorElement.textContent = "This field is required";
+
     }
-    
-    errorElement.textContent = "";
-    return true;
+    // if the field has the wrong format
+    if (field.validity.typeMismatch) {
+      errorElement.textContent = "Please enter a valid email address";
+
+    }
+    return false;
+  }
+
+  // if we get this far, then all fields are valid
+  errorElement.textContent = "";
+  return true;
 }
 
+// add an event listener to all fields to validate when the field is no longer in focus
 form.querySelectorAll("input, textarea").forEach((input) => {
-    input.addEventListener("blur", () => {
-        validateField(input);
-    });
+  input.addEventListener("blur", () => {
+    validateField(input);
+  });
 });
 
+// Add event listener to the form to check if all fields are valid on submit
+form.addEventListener("submit", function (e) {
+  // first, prevent the submission while we check form validation
+  e.preventDefault();
 
-form.addEventListener("submit", function(e) {
-    e.preventDefault();
-    
-    let isValid=true;
-    
-    const fields = form.querySelectorAll("input, textarea");
-    
-    fields.forEach((field) => {
-        const fieldValid = validateField(field);
-        
-        if(!fieldValid) {
-            isValid = false;
-        }
-    });
-    
-    if(isValid) {
-        form.submit();
+  let isValid = true;
+
+  // get a list of all form fields
+  const fields = form.querySelectorAll("input, textarea");
+
+  // for each form field...
+  fields.forEach((field) => {
+    // ...check that the field is valid
+    const fieldValid = validateField(field);
+
+    // if any field is not valid
+    if (!fieldValid) {
+      // this will prevent the submission
+      isValid = false;
     }
-    else {
-        form.querySelector(":invalid").focus();
-    }
+  });
+
+  // if all fields are valid then submit the form
+  if (isValid) {
+    form.submit();
+  }
+  // otherwise, set the cursor focus to the first invalid field
+  else {
+    form.querySelector(":invalid").focus();
+  }
 });
